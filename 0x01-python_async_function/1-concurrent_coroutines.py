eb02e9a96245630b8c17fcade1d30f2b6a62e60e
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-08-20 02:52:39 +0000

    update

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+"""
+1-concurrent_coroutines.py
+
+This module contains an async routine `wait_n` that spawns `wait_random`
+n times with the specified max_delay and returns the list of all the delays
+in ascending order.
+"""
+
 import asyncio
 from typing import List
 from basic_async_syntax import wait_random
@@ -6,16 +14,18 @@ from basic_async_syntax import wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Executes wait_random n times and returns a list in ascending order.
+    Spawns `wait_random` n times with the specified max_delay.
 
     Args:
-        n (int): The number of times to call wait_random.
-        max_delay (int): The maximum delay for wait_random.
+        n (int): The number of times to spawn `wait_random`.
+        max_delay (int): The maximum delay for `wait_random`.
 
     Returns:
-        List[float]: A list of delays in ascending order.
+        List[float]: A list of all delays in ascending order.
     """
-    delays = await asyncio.gather(
-        *(wait_random(max_delay) for _ in range(n))
-    )
+    delays = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+
     return sorted(delays)
---
+++ 0x01-python_async_function/basic_async_syntax.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+0-basic_async_syntax.py
+
+This module contains a single coroutine `wait_random` that waits for a random
+delay between 0 and max_delay seconds and returns the delay time.
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Waits for a random delay between 0 and max_delay seconds and returns
+    the actual delay.
+
+    Args:
+        max_delay (int): The maximum delay time in seconds (inclusive).
+
+    Returns:
+        float: The actual delay time.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
